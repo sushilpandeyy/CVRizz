be56543aa3ee277c8a452ff93282334e7a9451b6
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-18 16:05:52 +0530

    Added Layout

---
+++ app/s/contactus/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CVRIZZ",
+  description: "Leverage the power of AI to create beautiful resumes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en"> 
+      <body className={inter.className}>{children}
+      <Analytics />
+      <SpeedInsights/>
+      </body>
+    </html>
+  );
+}
