952d2ee18e5498f6186bd5ad39a49a72452dcece
Contactsushil <contact.sushilpandey@gmail.com>
2024-04-21 14:30:14 +0530

    Built Heading & View Options

---
+++ app/Resume/New/Page.tsx
@@ -0,0 +1,10 @@
+import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
+import Link from "next/link";
+
+export default function New(){
+    return (
+        <>
+        
+        </>
+    )
+}
--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -1,11 +1,55 @@
 "use client";
+import { GridIcon, ListBulletIcon } from "@radix-ui/react-icons";
 import Header from "../components/ui/Header";
+import {
+    Menubar,
+    MenubarMenu,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+
 
 export default function Resume(){
+    function MenubarDemo() {
+        return (
+          <Menubar>
+           <MenubarMenu>
+              <MenubarTrigger>
+                <GridIcon/> <p className="pl-2 pr-2">Grid</p>
+              </MenubarTrigger>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>
+                <ListBulletIcon/> <p className="pl-2 pr-2">List</p>
+              </MenubarTrigger>
+            </MenubarMenu>
+          </Menubar>
+        )
+      }
+    function Heading(){
+        return (
+            <>
+             <div className="flex justify-between content-center">
+            <div>
+            <h2 className="scroll-m-20 text-3xl tracking-tight">
+            RESUME<br/></h2>
+            </div>
+            <div className="flex items-center justify-center">
+            <div className="flex items-center justify-between">
+                <MenubarDemo/>
+            </div>
+            </div>
+            </div>
+            </>
+        )
+    }
+
+    
     return(
         <>
         <Header/>
-        
+        <div className="mx-8 mt-3">
+        <Heading/>
+        </div>
         </>
     )
 }
